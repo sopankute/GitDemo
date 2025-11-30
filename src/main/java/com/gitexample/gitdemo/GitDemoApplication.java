79dc1f7dc8891bb5087d683f960d873d9e444c0b
Sopan Kute <sopankutew@gmail.com>
2025-11-30 12:03:24 +0530

    Created GitDemo and added GitDemoProgram Util

---
+++ src/main/java/com/gitexample/gitdemo/GitDemoApplication.java
@@ -0,0 +1,18 @@
+package com.gitexample.gitdemo;
+
+import com.gitexample.gitdemo.util.GitDemoProgram;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+
+		GitDemoProgram gitDemoProgram = new GitDemoProgram();
+		gitDemoProgram.print();
+	}
+
+}
---
+++ src/main/java/com/gitexample/gitdemo/util/GitDemoProgram.java
@@ -0,0 +1,12 @@
+package com.gitexample.gitdemo.util;
+
+import org.springframework.stereotype.Component;
+
+
+public class GitDemoProgram {
+
+    public void print(){
+        System.out.println("\n GitDemoProgram.print \n");
+    }
+
+}
